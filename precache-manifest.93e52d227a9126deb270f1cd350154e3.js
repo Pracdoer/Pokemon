ff923f7bc6dd0d2295cf7127bcf523b3f032f1ff
Pracdoer <pracdoer@gmail.com>
2020-03-06 05:44:43 +0500

    Updates

---
+++ precache-manifest.93e52d227a9126deb270f1cd350154e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4e2f93b0ac05b6a40afa679a762740",
+    "url": "/git@github.com:Pracdoer/pokemon.git/index.html"
+  },
+  {
+    "revision": "a55297bc681bb6a5dd94",
+    "url": "/git@github.com:Pracdoer/pokemon.git/static/css/main.b1d666b4.chunk.css"
+  },
+  {
+    "revision": "48c51d7fdd1cb2d075c7",
+    "url": "/git@github.com:Pracdoer/pokemon.git/static/js/2.81e73034.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/git@github.com:Pracdoer/pokemon.git/static/js/2.81e73034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55297bc681bb6a5dd94",
+    "url": "/git@github.com:Pracdoer/pokemon.git/static/js/main.bfedd49c.chunk.js"
+  },
+  {
+    "revision": "1350b7fa68c91f4b5c72",
+    "url": "/git@github.com:Pracdoer/pokemon.git/static/js/runtime-main.5403b474.js"
+  }
+]);
